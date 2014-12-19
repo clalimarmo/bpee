c9f9dd68f9369235f0aa226928969cc0573aac58
Carlos Lalimarmo <clalimarmo@gmail.com>
2014-12-19 14:33:54 -0500

    Cut down coffee logger API
    -remove unused functionality
    -remove it's responsibility for knowing about files
    -inject file abstraction as dependency
    
    We only want the coffee logger to be about getting log data, and
    updating log data. If it doesn't need to know about *how* the file
    context switches (e.g. filenames and display names), just *when* the
    file context switches, then we can really simplify this layer, and
    clarify the division of responsibility in the app.
    
    We also cut out "reviews", since they aren't being used yet. There's
    also no reason to use an object to store records. An array works just as
    well, and we don't have to track the latest key value manually.

--- app/coffee_log/coffee_logger.js
+++ app/coffee_log/coffee_logger.js
@@ -1,77 +1,47 @@
 define(function() {
   var CoffeeLogger = function(deps) {
-    var datastore = deps.datastore;
-    var filename = deps.file.name;
-    var fileDisplayName = deps.file.displayName;
+    var file = deps.file;
 
     var instance = {};
 
-    var history = {};
-    var reviews = {};
+    var history = [];
     var lastHistoryRecordId = 0;
 
     var updatedCallbacks = [];
 
     var initialize = function() {
-      instance.onUpdated(deps.onFetched);
-      fetch();
+      file.onLoad(onFileLoad);
+    };
+
+    var runUpdatedCallbacks = function() {
+      for (var i in updatedCallbacks) {
+        updatedCallbacks[i]();
+      }
+    };
+
+    var exportData = function() {
+      return { history: history };
+    };
+
+    var importData = function(data) {
+      history = data.history;
+    };
+
+    var onFileLoad = function(data) {
+      importData(data);
+      runUpdatedCallbacks();
     };
 
     var save = function() {
-      var data = {
-        fileDisplayName: fileDisplayName,
-        history: history,
-        reviews: reviews,
-        lastHistoryRecordId: lastHistoryRecordId,
-      };
-      var metadata = {
-        displayName: fileDisplayName,
-      };
-      datastore.put(filename, data, metadata);
-    };
-
-    var fetch = function() {
-      datastore.get(filename, {
-        error: save,
-        success: function(data) {
-          fileDisplayName = data.fileDisplayName;
-          history = data.history;
-          reviews = data.reviews;
-          lastHistoryRecordId = data.lastHistoryRecordId;
-          for (var i in updatedCallbacks) {
-            updatedCallbacks[i]();
-          }
-        },
-      });
-    };
-
-    var reviewsFor = function(recordID) {
-      if (reviews[recordID] === undefined) {
-        reviews[recordID] = [];
-      }
-      return reviews[recordID];
-    };
-
-    instance.filename = function() {
-      return filename;
+      file.save(exportData());
     };
 
     instance.history = function() {
       return history;
     };
 
-    instance.reviews = function() {
-      return reviews;
-    };
-
     instance.addRecord = function(record) {
-      lastHistoryRecordId++;
-      history[lastHistoryRecordId] = record;
-      save();
-    };
-
-    instance.reviewRecord = function(recordId, review) {
-      reviewsFor(recordId).push(review);
+      history.push(record);
       save();
     };
 
@@ -80,13 +50,6 @@ define(function() {
         updatedCallbacks.push(callback);
       }
     };
-
-    instance.open = function(_filename) {
-      filename = _filename;
-      fetch();
-    };
-
-    instance.fetch = fetch;
 
     initialize();
     return instance;
